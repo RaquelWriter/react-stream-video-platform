b024456ad126c163bc8df40f6bf2f4b87010770e
RaquelWriter <raquel@mundofeliz.com>
2023-03-20 19:53:00 +0100

    feat: pink circle for custom cursor

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Player from './Player';
 import './App.css';
+import CustomCursor from 'custom-cursor-react';
+import 'custom-cursor-react/dist/index.css';
 
-function App() {
+/* function App() {
   return (
     <Router>
       <Routes>
@@ -13,5 +15,28 @@ function App() {
       </Routes>
     </Router>
   );
-}
+} */
+const App = () => (
+  <div>
+    <CustomCursor
+      targets={['.Link', '.thumb']}
+      customClass='custom-cursor'
+      dimensions={60}
+      fill='#FFF'
+      smoothness={{
+        movement: 0.5,
+        scale: 0.5,
+        opacity: 0.1,
+      }}
+      targetOpacity={0.5}
+    />
+    <Router>
+      <Routes>
+        <Route path='/' exact element={<Home />}></Route>
+        <Route path='/player/:id' element={<Player />}></Route>
+      </Routes>
+    </Router>
+  </div>
+);
+
 export default App;
